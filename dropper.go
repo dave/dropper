f6aee6ed84d26906de8b7deedeb7f16c81cca907
Dave Brophy <dave@brophy.uk>
2018-02-25 20:03:47 +0100

    Support directores

--- dropper.go
+++ dropper.go
@@ -1,30 +1,81 @@
 package dropper
 
 import (
+	"path/filepath"
+
+	"sync"
+
 	"github.com/MJKWoolnough/gopherjs/files"
+	"github.com/gopherjs/gopherjs/js"
 	"honnef.co/go/js/dom"
 )
 
-func Initialise() (enter, leave chan struct{}, drop chan []files.File) {
+type File struct {
+	files.File
+	dir string
+}
+
+func (f File) Path() string {
+	return filepath.Join(f.dir, f.Name())
+}
+
+func (f File) Reader() *files.FileReader {
+	return files.NewFileReader(f.File)
+}
+
+func Initialise() (enter, leave chan struct{}, drop chan []File) {
 	var window = dom.GetWindow()
 	var document = window.Document().(dom.HTMLDocument)
 	enter = make(chan struct{})
 	leave = make(chan struct{})
-	drop = make(chan []files.File)
+	drop = make(chan []File)
 	document.AddEventListener("drop", true, func(ev dom.Event) {
 		ev.PreventDefault()
-		dtf := ev.Underlying().Get("dataTransfer").Get("files")
-		var out []files.File
-		for i := 0; i < dtf.Length(); i++ {
-			file := files.NewFile(&dom.File{Object: dtf.Index(i)})
-			out = append(out, file)
+		items := ev.Underlying().Get("dataTransfer").Get("items")
+		var out []File
+		var wait sync.WaitGroup
+		var processEntry func(string, *js.Object)
+		processEntry = func(dir string, entry *js.Object) {
+			if entry.Get("isFile").Bool() {
+				wait.Add(1)
+				entry.Call("file", func(f *js.Object) {
+					file := File{
+						File: files.NewFile(&dom.File{Object: f}),
+						dir:  dir,
+					}
+					out = append(out, file)
+					wait.Done()
+				})
+			} else {
+				sub := filepath.Join(dir, entry.Get("name").String())
+				wait.Add(1)
+				entry.Call("createReader").Call("readEntries", func(entries []*js.Object) {
+					for _, child := range entries {
+						processEntry(sub, child)
+					}
+					wait.Done()
+				})
+			}
 		}
-		select {
-		case drop <- out:
-			// great!
-		default:
-			// nothing was listening.
+		for i := 0; i < items.Length(); i++ {
+			item := items.Index(i)
+			var entry *js.Object
+			if item.Get("getAsEntry").Bool() {
+				entry = item.Call("getAsEntry")
+			} else if item.Get("webkitGetAsEntry").Bool() {
+				entry = item.Call("webkitGetAsEntry")
+			}
+			processEntry("/", entry)
 		}
+		go func() {
+			wait.Wait()
+			select {
+			case drop <- out:
+				// great!
+			default:
+				// nothing was listening.
+			}
+		}()
 	})
 	document.AddEventListener("dragover", true, func(ev dom.Event) {
 		ev.PreventDefault()
